d44ecd0b27e591429893e4b870713e245a3c46f9
Keränen Pasi <pasi.keranen@digia.com>
2013-09-25 14:31:54 +0300

    Renaming declarative base class.
    Change-Id: Ib159ca18fc797300d2962f7ac677a33607f50c44
    Reviewed-by: Pasi Keränen <pasi.keranen@digia.com>
    Reviewed-by: Miikka Heikkinen <miikka.heikkinen@digia.com>

--- src/datavisualizationqml2/datavisualizationqml2_plugin.cpp
+++ src/datavisualizationqml2/datavisualizationqml2_plugin.cpp
@@ -41,8 +41,8 @@ void Datavis3Dqml2Plugin::registerTypes(const char *uri)
                                                   QLatin1String("Trying to create uncreatable: ScatterDataProxy."));
     qmlRegisterUncreatableType<QSurfaceDataProxy>(uri, 1, 0, "SurfaceDataProxy",
                                                   QLatin1String("Trying to create uncreatable: SurfaceDataProxy."));
-    qmlRegisterUncreatableType<AbstractDeclarative>(uri, 1, 0, "AbstractDeclarative",
-                                                  QLatin1String("Trying to create uncreatable: AbstractDeclarative."));
+    qmlRegisterUncreatableType<AbstractDeclarative>(uri, 1, 0, "AbstractGraph3D",
+                                                  QLatin1String("Trying to create uncreatable: AbstractGraph3D."));
 
     qmlRegisterType<QItemModelBarDataMapping>(uri, 1, 0, "BarDataMapping");
     qmlRegisterType<QItemModelScatterDataMapping>(uri, 1, 0, "ScatterDataMapping");
