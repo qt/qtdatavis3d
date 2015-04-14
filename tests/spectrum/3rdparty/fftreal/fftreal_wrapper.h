273bae38cf4f9b0234839ce1a0443ce5ea4e1066
Mika Salmela <mika.salmela@theqtcompany.com>
2015-04-14 13:35:51 +0300

    Copyright header changes
    Copyright header changes for 5.5.
    
    Change-Id: I0361cfe1ebdb28955cb927700f14cf7b72694421
    Reviewed-by: Miikka Heikkinen <miikka.heikkinen@theqtcompany.com>

--- tests/spectrum/3rdparty/fftreal/fftreal_wrapper.h
+++ tests/spectrum/3rdparty/fftreal/fftreal_wrapper.h
@@ -1,6 +1,6 @@
 /***************************************************************************
 **
-** Copyright (C) 2014 Digia Plc and/or its subsidiary(-ies).
+** Copyright (C) 2015 The Qt Company Ltd and/or its subsidiary(-ies).
 ** Contact: http://www.qt-project.org/legal
 **
 ** This file is part of the examples of QtDataVisualization.
