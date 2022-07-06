336135cb91d52e17b8fc20273a6749b368f5952f
Lucie Gérard <lucie.gerard@qt.io>
2022-07-06 12:49:11 +0200

    Add license headers to cmake files
    CMakeLists.txt and .cmake files of significant size
    (more than 2 lines according to our check in tst_license.pl)
    now have the copyright and license header.
    
    Existing copyright statements remain intact
    
    Task-number: QTBUG-88621
    Change-Id: I829aeda2bda017b7d5bf39795b79dbaee143cb60
    Reviewed-by: Jörg Bornemann <joerg.bornemann@qt.io>

--- src/datavisualization/configure.cmake
+++ src/datavisualization/configure.cmake
@@ -1,3 +1,6 @@
+# Copyright (C) 2022 The Qt Company Ltd.
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+
 qt_extra_definition("QT_VERSION_STR" "\"${PROJECT_VERSION}\"" PUBLIC)
 qt_extra_definition("QT_VERSION_MAJOR" ${PROJECT_VERSION_MAJOR} PUBLIC)
 qt_extra_definition("QT_VERSION_MINOR" ${PROJECT_VERSION_MINOR} PUBLIC)
