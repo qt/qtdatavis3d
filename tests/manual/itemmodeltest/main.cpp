8a273e7a7fd9b3738d1b0c429df3504696435ff7
Tomi Korpipaa <tomi.korpipaa@qt.io>
2024-02-13 10:28:44 +0200

    Fix invalid manual test
    Fixes: QTBUG-122185
    Change-Id: Id55e7731b5e58614bcae28ce48895ce175e2cc1f
    Reviewed-by: Sami Varanka <sami.varanka@qt.io>
    Reviewed-by: Tomi Korpipää <tomi.korpipaa@qt.io>
    (cherry picked from commit 41a23e6ad9783a46f740fcfc04dc3c3e17a9005a)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/manual/itemmodeltest/main.cpp
+++ tests/manual/itemmodeltest/main.cpp
@@ -266,7 +266,7 @@ int main(int argc, char **argv)
     barProxy->setValueRoleReplace(QStringLiteral("\\4"));
     barProxy->setRotationRoleReplace(QStringLiteral("\\5"));
     surfaceProxy->setXPosRole(tableWidget->model()->roleNames().value(Qt::DisplayRole));
-    surfaceProxy->setZPosRole(tableWidget->model()->roleNames().value(Qt::DisplayRole));
+    surfaceProxy->setYPosRole(tableWidget->model()->roleNames().value(Qt::DisplayRole));
     surfaceProxy->setXPosRolePattern(QRegularExpression(QStringLiteral("^(\\d*)\\/(\\d*)\\/\\d*[\\.\\,]?\\d*\\/\\d*[\\.\\,]?\\d*$")));
     surfaceProxy->setXPosRoleReplace(QStringLiteral("\\2"));
     surfaceProxy->setYPosRolePattern(QRegularExpression(QStringLiteral("^\\d*(\\/)(\\d*)\\/(\\d*[\\.\\,]?\\d*)\\/\\d*[\\.\\,]?\\d*$")));
