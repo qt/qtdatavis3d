0b7b970bdd123694ee54d0225e558b650ddd2f22
Tarja Sundqvist <tarja.sundqvist@qt.io>
2025-05-16 11:47:17 +0300

    Update commercial SPDX-License-Identifier
    Updated the commercial SPDX-License-Identifier to
    the source files. Examples, tests, documentation,
    or build files are not updated.
    
    Modified licenseRule.json and REUSE.toml accordingly.
    
    Task-number: QTBUG-136484
    Change-Id: I2fe010721f0fd27b54014a7cc33cd41e69a7a575
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- src/datavisualization/axis/qabstract3daxis.cpp
+++ src/datavisualization/axis/qabstract3daxis.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2017 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial
 
 #include "qabstract3daxis_p.h"
 
--- src/datavisualization/utils/qutils.h
+++ src/datavisualization/utils/qutils.h
@@ -1,5 +1,5 @@
 // Copyright (C) 2016 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial
 
 #ifdef QTGRAPHS_QUTILS_H
 #error Mixing QtDataVisualization and QtGraphs in the same TU is unsupported since they use\
